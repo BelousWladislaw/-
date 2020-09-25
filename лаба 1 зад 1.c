9eb811bedbd4e0fcc03527ca8513c5ab93036463
BelousWladislaw <71496075+BelousWladislaw@users.noreply.github.com>
2020-09-25 13:52:46 +0300

    Add files via upload

---
+++ лаба 1 зад 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+int main() {
+    
+    float x,y;
+   
+    printf("enter x: ");
+    scanf("%f",&x);
+
+
+    y=4*x*x*x;
+    printf("4x^3=%f\n",y);
+    system("pause");
+    return 0;
+}
+
+    
+
